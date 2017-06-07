289f1189b071e35376aa3d21b1c3f2729e139170
Gamerhythm <jake@gealer.email>
2017-06-07 16:06:43 +0100

    Converted spaces to tabs

--- discrimy.py
+++ discrimy.py
@@ -9,30 +9,30 @@ import pip
 import asyncio
 
 if os.path.isfile("temp.py"):
-    os.remove("temp.py")
+	os.remove("temp.py")
 
 if not os.path.isfile("discrimlist.ini"):
-    raise Exception("[ERROR] Can't find the discriminator list.")
+	raise Exception("[ERROR] Can't find the discriminator list.")
 
 try:
-    shutil.copyfile("config.ini", "temp.py")
-    from temp import *
-    os.remove("temp.py")
+	shutil.copyfile("config.ini", "temp.py")
+	from temp import *
+	os.remove("temp.py")
 except:
-    raise Exception("[ERROR] Config load error. Probably missing, broken or in a directory with no write permissions.")
+	raise Exception("[ERROR] Config load error. Probably missing, broken or in a directory with no write permissions.")
 
 if discordtoken == "" and discordpass == "":
-    raise Exception("[ERROR] Config not edited.")
+	raise Exception("[ERROR] Config not edited.")
 
 def install(package):
-    pip.main(['install', package])
+	pip.main(['install', package])
 
 import logging
 try:
-    import discord
+	import discord
 except:
-    install("discord.py")
-    raise Exception("Discord recently installed. Rerun this application.")
+	install("discord.py")
+	raise Exception("Discord recently installed. Rerun this application.")
 
 print("[INFO] Signing into Discord. This may take a while.")
 logging.basicConfig(level=logging.ERROR)
@@ -40,83 +40,83 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    OriginalName = client.user.name
-    print("\n------------------")
-    print('Logged in as ' + OriginalName)
-    print('ID: ' + client.user.id)
-    print("------------------\n")
-    MemberCount = 0
-    ServerCount = 0
-    UserList = []
-    for server in client.servers:
-        ServerCount = ServerCount + 1
-        try:
-            print("[SERVER] " + str(server.name))
-        except:
-            print("[SERVER] Name failed to load!")
-        for member in server.members:
-            MemberCount = MemberCount + 1
-            UserList.append(member)
-    if ServerCount == 1:
-        print("[MEMBERS] There are approximately " + str(MemberCount) + " members in the 1 server you are in!")
-        if MemberCount > 30000:
-           print("[INFO] You are only in one server, but that server appears to have over 30,000 users. You should be fine, but if you encounter issues, join another big server.") 
-    else:
-        print("[MEMBERS] There are approximately " + str(MemberCount) + " members in the " + str(ServerCount) +  " servers you are in!")
-    if MemberCount < 30000:
-        print("[WARNING] There are under 30,000 members in the combined servers you are in. This may cause issues with the discriminator editing process. We recommend joining a few big guilds such as the Discord API server.")
-    print("\n------------------\n")
-    while True:
-        discrimlist = []
-        with open("discrimlist.ini") as d:
-            discrimlist = d.readlines()
-        discrimlist = [l.strip() for l in discrimlist]
-        ndl = []
-        for discrim in discrimlist:
-            discrimsplit = list(discrim)
-            nds = []
-            charlength = 0
-            for char in discrimsplit:
-                charlength = charlength + 1
-                if char == "*":
-                    char = client.user.discriminator[charlength - 1]
-                if char == "%":
-                    char = client.user.discriminator[0]
-                nds.append(char)
-            ndl.append(''.join(nds))
-        discrimlist = ndl
-        AlreadyGotDiscrim = False
-        for discrim in discrimlist:
-            if discrim == client.user.discriminator:
-                AlreadyGotDiscrim = True
-        if not AlreadyGotDiscrim:
-            FoundNickname = False
-            DiscrimName = ""
-            OldDiscrim = client.user.discriminator
-            for user in UserList:
-                if FoundNickname == False:
-                    if not client.user.name == user.name and client.user.discriminator == user.discriminator:
-                        FoundNickname = True
-                        DiscrimName = user.name
-            if FoundNickname == False:
-                print("[ERROR] Farming could not continue because another user could not be found with the same discriminator. Try joining some more big servers.")
-            else:
-                await client.edit_profile(password=discordpass, username=DiscrimName)
-                if ChangeNicknameBack:
-                    await client.edit_profile(password=discordpass, username=OriginalName)
-                    print("[INFO] Username changed and then changed back. This has changed your discriminator.")
-                else:
-                    print("[INFO] Username/discriminator changed.")
-                await asyncio.sleep(3)
-                if not client.user.discriminator == OldDiscrim:
-                    print("[INFO] The new discriminator is #" + client.user.discriminator)
-                else:
-                    print("[INFO] Due to a delay between you and the Discord API, your new discriminator will not be displayed.")
-        else:
-            print("[INFO] You appear to already have a discriminator you want. Farming has stopped.")
-        if ChangeNicknameBack:
-            await asyncio.sleep(3597)
-        else:
-            await asyncio.sleep(1797)
+	OriginalName = client.user.name
+	print("\n------------------")
+	print('Logged in as ' + OriginalName)
+	print('ID: ' + client.user.id)
+	print("------------------\n")
+	MemberCount = 0
+	ServerCount = 0
+	UserList = []
+	for server in client.servers:
+		ServerCount = ServerCount + 1
+		try:
+			print("[SERVER] " + str(server.name))
+		except:
+			print("[SERVER] Name failed to load!")
+		for member in server.members:
+			MemberCount = MemberCount + 1
+			UserList.append(member)
+	if ServerCount == 1:
+		print("[MEMBERS] There are approximately " + str(MemberCount) + " members in the 1 server you are in!")
+		if MemberCount > 30000:
+			print("[INFO] You are only in one server, but that server appears to have over 30,000 users. You should be fine, but if you encounter issues, join another big server.") 
+	else:
+		print("[MEMBERS] There are approximately " + str(MemberCount) + " members in the " + str(ServerCount) +  " servers you are in!")
+	if MemberCount < 30000:
+		print("[WARNING] There are under 30,000 members in the combined servers you are in. This may cause issues with the discriminator editing process. We recommend joining a few big guilds such as the Discord API server.")
+	print("\n------------------\n")
+	while True:
+		discrimlist = []
+		with open("discrimlist.ini") as d:
+			discrimlist = d.readlines()
+		discrimlist = [l.strip() for l in discrimlist]
+		ndl = []
+		for discrim in discrimlist:
+			discrimsplit = list(discrim)
+			nds = []
+			charlength = 0
+			for char in discrimsplit:
+				charlength = charlength + 1
+				if char == "*":
+					char = client.user.discriminator[charlength - 1]
+				if char == "%":
+					char = client.user.discriminator[0]
+				nds.append(char)
+			ndl.append(''.join(nds))
+		discrimlist = ndl
+		AlreadyGotDiscrim = False
+		for discrim in discrimlist:
+			if discrim == client.user.discriminator:
+				AlreadyGotDiscrim = True
+		if not AlreadyGotDiscrim:
+			FoundNickname = False
+			DiscrimName = ""
+			OldDiscrim = client.user.discriminator
+			for user in UserList:
+				if FoundNickname == False:
+					if not client.user.name == user.name and client.user.discriminator == user.discriminator:
+						FoundNickname = True
+						DiscrimName = user.name
+			if FoundNickname == False:
+				print("[ERROR] Farming could not continue because another user could not be found with the same discriminator. Try joining some more big servers.")
+			else:
+				await client.edit_profile(password=discordpass, username=DiscrimName)
+				if ChangeNicknameBack:
+					await client.edit_profile(password=discordpass, username=OriginalName)
+					print("[INFO] Username changed and then changed back. This has changed your discriminator.")
+				else:
+					print("[INFO] Username/discriminator changed.")
+				await asyncio.sleep(3)
+				if not client.user.discriminator == OldDiscrim:
+					print("[INFO] The new discriminator is #" + client.user.discriminator)
+				else:
+					print("[INFO] Due to a delay between you and the Discord API, your new discriminator will not be displayed.")
+		else:
+			print("[INFO] You appear to already have a discriminator you want. Farming has stopped.")
+		if ChangeNicknameBack:
+			await asyncio.sleep(3597)
+		else:
+			await asyncio.sleep(1797)
 
 client.run(discordtoken, bot=False)
